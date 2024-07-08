afc400dd9f2d35ba6cfc8bf9ac70e7d92dbf0e00
asemsqa <asesqa@gmail.com>
2024-07-08 11:16:22 +0200

    Add representation page

--- src/app/pages/account/representations/representations.component.ts
+++ src/app/pages/account/representations/representations.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -6,6 +6,22 @@ import { CommonModule } from '@angular/common';
   templateUrl: './representations.component.html',
   styleUrl: './representations.component.scss'
 })
-export class RepresentationsComponent {
+export class RepresentationsComponent implements OnInit{
+
+  representations: any;
+  email:any;
+  firstname:any;
+  lastname:any;
+  address:any;
+  document_number:any;
+
+  constructor(){}
+  ngOnInit(): void {
+
+  }
+
+  registerRepresentation(){
+    console.log("Go to register representation");
+  }
 
 }
