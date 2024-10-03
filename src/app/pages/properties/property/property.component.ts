aa613f0fb73fe6081dc45a0a5b4f63184ea94e09
jorgeceroideas <jorge@ceroideas.es>
2024-10-03 22:06:25 +0200

    cambiando fecha

--- src/app/pages/properties/property/property.component.ts
+++ src/app/pages/properties/property/property.component.ts
@@ -179,7 +179,7 @@ export class PropertyComponent implements OnInit {
 
   public calculateLeftTime1(): void {
     const propertyCard: any = document.querySelector(`.left-time1-${ this.property.guid }`);
-    const timeToEnd: any = new Date(this.property.end_date.split("-").reverse().join("-"));    
+    const timeToEnd: any = new Date(this.property.end_date_original);
 
     const interval = setInterval(() => {
 
@@ -210,6 +210,7 @@ export class PropertyComponent implements OnInit {
         this.calculateLeftTime1();
       },1000);
       this.property.start_date = moment(this.property.start_date).format('DD-MM-YYYY');
+      this.property.end_date_original = this.property.end_date;
       this.property.end_date = moment(this.property.end_date).format('DD-MM-YYYY');
       this.embedVideo = this.property.videos.length ? this.embedService.embed(this.property.videos[1].link) : null;
       this.lat = +this.property.location.lat;
